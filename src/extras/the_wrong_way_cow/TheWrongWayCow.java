498a1ce9c41b8d3fa4d7654dec1b8cfc40b7aaea
alvincvms <48074117+alvincvms@users.noreply.github.com>
2020-05-12 18:04:51 -0700

    Finished the wrong way cow

--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -41,44 +41,86 @@
 
 package extras.the_wrong_way_cow;
 
+import java.util.HashMap;
+
 public class TheWrongWayCow {
 
     public static int[] findWrongWayCow(final char[][] field) {
         // Fill in the code to return the x,y coordinate position of the
         // head (letter 'c') of the wrong way cow!
-    	int upCows = 0, downCows = 0, rightCows = 0, leftCows = 0;
-    	//test for cows facing left or right
+    	
+    	HashMap<Integer, Integer> N = new HashMap<Integer, Integer>(), S = new HashMap<Integer, Integer>(), E = new HashMap<Integer, Integer>(), W = new HashMap<Integer, Integer>();
+    	//check for cows facing east or west
         for(int i = 0; i < field.length; i++) {
         	char[] s = field[i];
-        	System.out.println(new String(s));
+//        	System.out.println(new String(s));
         	for(int j = 0; j < s.length - "cow".length() + 1; j++) {
         		for(int n = 0; n < "cow".length(); n++) {
         			if(s[j + n] != "cow".charAt(n)) {
         				break;
         			}
         			if(n == "cow".length() - 1) {
-        				rightCows++;
+        				W.put(j, i);
         				j += "cow".length();
         				break;
         			}
         		}
         	}
-        	
-        	for(int j = s.length - 1; j >= "cow".length() - 1; j++) {
+        	for(int j = s.length - 1; j >= "cow".length() - 1; j--) {
         		for(int n = 0; n < "cow".length(); n++) {
         			if(s[j - n] != "cow".charAt(n)) {
         				break;
         			}
-        			fix this
         			if(n == "cow".length() - 1) {
-        				leftCows++;
+        				E.put(j, i);
         				j -= "cow".length();
         				break;
         			}
         		}
         	}
         }
-        System.out.println(rightCows + ", " + leftCows + ", " + upCows + ", " + downCows);
-        return null;
+        
+        //check for cows facing north or south
+        for(int i = 0; i < field[0].length; i++) {
+        	for(int j = 0; j < field.length - "cow".length() + 1; j++) {
+        		for(int n = 0; n < "cow".length(); n++) {
+        			if(field[j + n][i] != "cow".charAt(n)) {
+        				break;
+        			}
+        			if(n == "cow".length() - 1) {
+        				N.put(i, j);
+        				j += "cow".length();
+        				break;
+        			}
+        		}
+        	}
+        	for(int j = field.length - 1; j >= "cow".length() - 1; j--) {
+        		for(int n = 0; n < "cow".length(); n++) {
+        			if(field[j - n][i] != "cow".charAt(n)) {
+        				break;
+        			}
+        			if(n == "cow".length() - 1) {
+        				S.put(i, j);
+        				j -= "cow".length();
+        				break;
+        			}
+        		}
+        	}
+        }
+//      System.out.println(wCows + ", " + eCows + ", " + sCows + ", " + nCows);
+        if(N.size() == 1) {
+        	System.out.println((int) N.keySet().toArray()[0] + ", " + (int) N.values().toArray()[0]);
+        	return new int[] {(int) N.keySet().toArray()[0], (int) N.values().toArray()[0]};
+        }
+        if(S.size() == 1) {
+        	return new int[] {(int) S.keySet().toArray()[0], (int) S.values().toArray()[0]};
+        }
+        if(E.size() == 1) {
+        	return new int[] {(int) E.keySet().toArray()[0], (int) E.values().toArray()[0]};
+        }
+        if(W.size() == 1) {
+        	return new int[] {(int) W.keySet().toArray()[0], (int) W.values().toArray()[0]};
+        }
+        return new int[] {-1, -1};
     }
 }
